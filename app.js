6192ab2bf2066e7f660d8056da60d08afe4c963c
robo-stark <azreelro.khanbo@gmail.com>
2024-07-13 17:56:39 +0530

    cors added

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import './config/db.js';
+import cors from "cors";
 import routes from './routes/index.js';
 
 const bodyParser = express.json;
 
 
 const app = express();
+app.use(cors());
 app.use(express.json({}));
 app.use(express.json({ extended : true }));
 app.use(bodyParser());
